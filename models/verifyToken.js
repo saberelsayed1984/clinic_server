c59825e61baff232201973bc18a61d15559c1d14
saber <saberelsayed1984@gmail.com>
2024-03-26 11:45:31 +0200

    secound

--- models/verifyToken.js
+++ models/verifyToken.js
@@ -5,13 +5,13 @@ import dotenv from "dotenv";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 dotenv.config({ path: path.join(__dirname, "../.env") });
-console.log(      process.env.JWT_SECRET_KEY    );
+// console.log(      process.env.JWT_SECRET_KEY    );
 function verifyToken(req, res, next) {
   if (
     req.headers.authorization &&
     req.headers.authorization.split(' ')[0] === 'Bearer'
   ) {
-    console.log(req.headers.authorization);
+    // console.log(req.headers.authorization);
     const decoded = 
     jwt.verify(
       req.headers.authorization.split(' ')[1],
