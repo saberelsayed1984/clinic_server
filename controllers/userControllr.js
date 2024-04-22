49083769286ed59b4600c55762fd584368e049cb
saber <saberelsayed1984@gmail.com>
2024-04-22 02:23:42 +0200

    six

--- controllers/userControllr.js
+++ controllers/userControllr.js
@@ -319,32 +319,32 @@ export async function forgotPassword(req, res, next) {
         expiresIn: '60m'
     });
 
-    const mail = "team62024@outlook.com" ;
-     const pass ="yrbmmqddqvnzalii";
+    // const mail = "team62024@outlook.com" ;
+    //  const pass ="yrbmmqddqvnzalii";
     const link = 
-    `https://clinic-server-1.onrender.com/password/resetpassword/${user._id}/${token}`;
-    const transporter = nodemailer.createTransport({
-        service: "hotmail",
-        auth: {
-            user: mail,
-            pass: pass,
-        }
-    });
-    const mailOption = {
-        from: '"Medi Team"<team62024@outlook.com>',
-        to: user.email,
-        subject: "Reset your password",
-        text: `Please click on the following link to reset your password: ${link}`
-    }
-    transporter.sendMail(mailOption, (error , success) =>{
-        if (error){
-            console.log(error);
-        }else{
-            console.log("email was sent: " + success.response)
-        }
+    ` http://localhost:5000/password/resetpassword/${user._id}/${token}`;
+    // const transporter = nodemailer.createTransport({
+    //     service: "hotmail",
+    //     auth: {
+    //         user: mail,
+    //         pass: pass,
+    //     }
+    // });
+    // const mailOption = {
+    //     from: '"Medi Team"<team62024@outlook.com>',
+    //     to: user.email,
+    //     subject: "Reset your password",
+    //     text: `Please click on the following link to reset your password: ${link}`
+    // }
+    // transporter.sendMail(mailOption, (error , success) =>{
+    //     if (error){
+    //         console.log(error);
+    //     }else{
+    //         console.log("email was sent: " + success.response)
+    //     }
     
-    });
-    res.send({msg : `Please click on the following link to reset your password: ${link}`} )
+    // });
+    res.render("viwe-link.ejs" )
     }
 
 export async function getResetPassword(req, res, next) {
