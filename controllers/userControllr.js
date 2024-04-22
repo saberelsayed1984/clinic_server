3615c293db434c11d9accb23eb0359383689017a
saber <saberelsayed1984@gmail.com>
2024-04-22 03:57:26 +0200

    seven

--- controllers/userControllr.js
+++ controllers/userControllr.js
@@ -319,29 +319,29 @@ export async function forgotPassword(req, res, next) {
         expiresIn: '60m'
     });
 
-    const mail = "team62024@outlook.com" ;
-     const pass ="yrbmmqddqvnzalii";
-    const transporter = nodemailer.createTransport({
-        service: "hotmail",
-        auth: {
-            user: mail,
-            pass: pass,
-        }
-    });
-    const mailOption = {
-        from: '"Medi Team"<team62024@outlook.com>',
-        to: user.email,
-        subject: "Reset your password",
-        text: `Please click on the following link to reset your password: ${link}`
-    }
-    transporter.sendMail(mailOption, (error , success) =>{
-        if (error){
-            console.log(error);
-        }else{
-            console.log("email was sent: " + success.response)
-        }
+    // const mail = "team62024@outlook.com" ;
+    //  const pass ="yrbmmqddqvnzalii";
+    // const transporter = nodemailer.createTransport({
+    //     service: "hotmail",
+    //     auth: {
+    //         user: mail,
+    //         pass: pass,
+    //     }
+    // });
+    // const mailOption = {
+    //     from: '"Medi Team"<team62024@outlook.com>',
+    //     to: user.email,
+    //     subject: "Reset your password",
+    //     text: `Please click on the following link to reset your password: ${link}`
+    // }
+    // transporter.sendMail(mailOption, (error , success) =>{
+    //     if (error){
+    //         console.log(error);
+    //     }else{
+    //         console.log("email was sent: " + success.response)
+    //     }
     
-    });
+    // });
     const link = `https://clinic-server-1.onrender.com/password/resetpassword/${user._id}/${token}`;
 
     res.render('viwe-mail.ejs',{link:link})
